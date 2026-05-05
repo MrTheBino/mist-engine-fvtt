22d80ea3616a4a5eb754823431ae72a99d9ceadb
Paul Umbers <paul.umbers@gmail.com>
2026-05-05 10:46:35 -0600

    Added example actors pack and sample journey

--- src/commands/pack.mjs
+++ src/commands/pack.mjs
@@ -4,5 +4,6 @@ console.log("working directory: " + process.cwd());
 //await compilePack("./src/packs/challenges", "./packs/challenges", { nedb: false,log: true });
 await compilePack("./src/packs/litm-themebooks", "./packs/litm-themebooks", { nedb: false,log: true });
 await compilePack("./src/packs/pregen-characters", "./packs/pregen-characters", { nedb: false,log: true });
+await compilePack("./src/packs/example-actors", "./packs/example-actors", { nedb: false,log: true });
 //await compilePack("./src/packs/pregen-heroes", "./packs/pregen-heroes", { nedb: false,log: true });
 await compilePack("./src/packs/system-documentation", "./packs/system-documentation", { nedb: false,log: true });
